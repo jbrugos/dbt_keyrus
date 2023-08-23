f4d9c2b7cefae47ccfee06f78aa53372a666890e
jbrugos@gmail.com <jbrugos@gmail.com>
2023-08-23 09:26:39 +0000

    Final staging commit

--- models/staging/shop/customers_view.sql
+++ models/staging/shop/customers_view.sql
@@ -1,9 +1,9 @@
-with raw as (
 
-    select 
-        id, 
-        first_name, 
-        last_name 
-    from {{ source('SnowKeyrus', 'customers') }}
-)
-select * from raw;
+
+with
+    raw as (
+
+        select id, first_name, last_name from {{ source("SnowKeyrus", "customers") }}
+    )
+select *
+from raw
--- models/staging/stripe/payment_view.sql
+++ models/staging/stripe/payment_view.sql
@@ -6,4 +6,3 @@ with
     )
 select *
 from rawp
-;
