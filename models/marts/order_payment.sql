f55785a7aede1208b9a8f6fb4da2ef4a430faaf6
jbrugos@gmail.com <jbrugos@gmail.com>
2023-09-18 13:57:52 +0000

    testing macros

--- models/marts/order_payment.sql
+++ models/marts/order_payment.sql
@@ -2,7 +2,7 @@
 
 with order_pay as (
 
-    select o.id, o.user_id, o.order_date, o.status, p.pay_id, p.pay_status, p.amount,p.paymentmethod, o._etl_loaded_at as order_load_time 
+    select o.id, o.user_id, o.order_date, o.status, p.pay_id, p.pay_status, p.amount, p.paymentmethod, o._etl_loaded_at as order_load_time 
     from {{ ref("orders_view") }} as o
     left join {{ ref("payment_view") }} as p
     on o.id=p.orderid
@@ -10,4 +10,4 @@ with order_pay as (
 
 select * from order_pay
 
-{{limit_data('order_date')}}
+{{ limit_data('order_date') }}
