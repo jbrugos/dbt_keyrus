d43d304c483322f4dc387b5afaa7d492659a91a0
jbrugos@gmail.com <jbrugos@gmail.com>
2023-09-27 10:01:57 +0000

    Stripe schema changed

--- models/staging/stripe/payment_view.sql
+++ models/staging/stripe/payment_view.sql
@@ -1,7 +1,7 @@
 with
     rawp as (
 
-        select pay_id, orderid, paymentmethod, status as pay_status, {{ cents_to_dollar('amount', 2) }} as amount, created, _batched_at
+        select pay_id, orderid, paymentmethod, pay_status, {{ cents_to_dollar('amount', 2) }} as amount, created, _batched_at
         from {{ source("striping", "payment") }}
     )
 -- This is only a macro to format the amount in dollar
