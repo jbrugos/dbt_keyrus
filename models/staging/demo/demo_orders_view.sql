5e10ae2b3d58a9efe37bd6d52e40c7e9971885a6
jbrugos@gmail.com <jbrugos@gmail.com>
2023-10-20 16:11:35 +0000

    demo mart

--- models/staging/demo/demo_orders_view.sql
+++ models/staging/demo/demo_orders_view.sql
@@ -1,7 +1,7 @@
 {{ config( materialized='view', schema='demo') }}
 
 With raw as (
-    select id, user_id, status, order_date, _etl_loaded_at 
+    select id, user_id, status, order_date, _etl_loaded_at, row_number() over (partition by user_id order by order_date, id) as user_order_seq
     from {{ source("demo_k", "demo_orders") }}
 )
 
