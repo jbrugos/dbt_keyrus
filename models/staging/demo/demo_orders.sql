de66b0b776b87817eaacac112955ffadf2505a88
jbrugos@gmail.com <jbrugos@gmail.com>
2023-10-20 10:44:17 +0000

    demo schema changes

--- models/staging/demo/demo_orders.sql
+++ models/staging/demo/demo_orders.sql
@@ -1,7 +1,7 @@
 
 With raw as (
     select id, user_id, status, order_date, _etl_loaded_at 
-    from {{ source("demo", "demo_orders") }}
+    from {{ source("demo_k", "demo_orders") }}
 )
 
 select * from orders
